2c3622e9faac917919c482b4135e53566cf21287
Samael Home <samaelortiz2218@gmail.com>
2025-06-09 18:18:50 -0700

    Creando un unico punto de acceso .htaccess

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titulo</title>
+</head>
+<body>
+    <h1>Unico punto de acceso</h1>
+    <p>El framework MVC (Modelo-Vista-Controlador) es una arquitectura de software que divide la lógica de una aplicación en tres componentes principales:</p>
+    <ul>
+        <li>Modelo (Model): Encarga de la lógica de negocio y la manipulación de datos.</li>
+        <li>Vista (View): Se encarga de mostrar la información al usuario.</li>
+        <li>Controlador (Controller): Actúa como intermediario entre el modelo y la vista, gestionando las solicitudes del usuario y las respuestas.</li>
+    </ul>
+</body>
+</html>
