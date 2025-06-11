ce7f64a03d8b53b296a8b4388264616023283b0b
Samael Home <samaelortiz2218@gmail.com>
2025-06-11 13:57:00 -0700

    Creando rutas para crud

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -11,4 +11,28 @@ class HomeController extends Controller
         
         return $task->all();
     }
+    public function show($id)
+    {
+        $task = new Task();
+        
+        return $task->find($id);
+    }
+    public function create($data)
+    {
+        $task = new Task();
+        
+        return $task->create($data);
+    }
+    public function update($id, $data)
+    {
+        $task = new Task();
+        
+        return $task->update($id, $data);
+    }
+    public function delete($id)
+    {
+        $task = new Task();
+        
+        return $task->delete($id);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,20 @@ use Lib\Route;
 use App\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
+//Mostrar todos
+Route::get('/contacts', [HomeController::class, 'index']);
+//Mostrar uno
+Route::get('/contacts/:id', [HomeController::class, 'show']);
+//Crear
+Route::get('/contacts/create', [HomeController::class, 'create']);
+//Guardar
+Route::post('/contacts', [HomeController::class, 'store']);
+// Editar
+Route::get('/contacts/:id/edit', [HomeController::class, 'edit']);
+// Actualizar
+Route::post('/contacts/:id', [HomeController::class, 'update']);
+// Eliminar
+Route::post('/contacts/:id/delete', [HomeController::class, 'delete']);
 
-Route::get('/contacto', function () {
-    return "Contacto";
-});
-Route::get('/cursos/:slug', function ($slug) {
-    return [
-        'curso' => $slug,
-        'precio' => 15000
-    ];
-});
 
 Route::dispatch();
